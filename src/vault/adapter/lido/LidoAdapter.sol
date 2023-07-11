082ca484bb48debb210c4fb0b2600afddbf17080
Volodymyr <zolotukhinvolodymyr@gmail.com>
2023-07-11 14:41:14 +0300

    fixes after conflicts

--- src/vault/adapter/lido/LidoAdapter.sol
+++ src/vault/adapter/lido/LidoAdapter.sol
@@ -18,13 +18,14 @@ import {SafeMath} from "openzeppelin-contracts/utils/math/SafeMath.sol";
 /// since this prevents attackers from atomically increasing the vault's share value
 /// and exploiting lending protocols that use this vault as a borrow asset.
 contract LidoAdapter is AdapterBase {
+    // using FixedPointMathLib for uint256;
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
     using Math for uint256;
 
     string internal _name;
     string internal _symbol;
-    
+
     int128 private constant WETHID = 0;
     int128 private constant STETHID = 1;
     ICurveMetapool public constant StableSwapSTETH =
@@ -32,28 +33,29 @@ contract LidoAdapter is AdapterBase {
     uint256 public constant DENOMINATOR = 10000;
     uint256 public slippage; // = 100; //out of 10000. 100 = 1%
 
-    ICurveMetapool public pool;
+    /// @notice The poolId inside Convex booster for relevant Curve lpToken.
+    uint256 public pid;
 
-    uint256 public constant BPS_DENOMINATOR = 10_000;
-
-    uint256 public slippage; // = 100; //out of 10000. 100 = 1%
-
-    address public stEth;
+    /// @notice The booster address for Convex
+    ILido public lido;
 
     // address public immutable weth;
-    IWETH public constant weth =
-        IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IWETH public weth;
+
+    address private referal = address(0); //stratms. for recycling and redepositing
+
+    // We need to figure out how to get this referral
 
     /*//////////////////////////////////////////////////////////////
                                 INITIALIZATION
   //////////////////////////////////////////////////////////////*/
 
-    error InvalidAsset();
-
     /**
      * @notice Initialize a new Lido Adapter.
      * @param adapterInitData Encoded data for the base adapter initialization.
      * @param lidoInitData Encoded data for the Lido adapter initialization.
+     * @dev `_lidoAddress` - The vault address for Lido.
+     * @dev `_pid` - The poolId for lpToken.
      * @dev This function is called by the factory contract when deploying a new vault.
      */
     function initialize(
@@ -67,28 +69,36 @@ contract LidoAdapter is AdapterBase {
             lidoInitData,
             (address, uint256)
         );
-        
-        if (asset() != address(weth)) revert InvalidAsset();
 
-        (
-            address _pool,
-            uint256 _wEthId,
-            uint256 _stEthId,
-            uint256 _slippage
-        ) = abi.decode(lidoInitData, (address, uint256, uint256, uint256));
+        lido = ILido(ILido(_lidoAddress).token());
+        pid = _pid;
+        weth = IWETH(ILido(_lidoAddress).weth());
+        slippage = 100;
 
-        stEth = _stEth;
+        _name = string.concat(
+            "Popcorn Lido ",
+            IERC20Metadata(address(weth)).name(),
+            " Adapter"
+        );
+        _symbol = string.concat(
+            "popL-",
+            IERC20Metadata(address(weth)).symbol()
+        );
 
-        pool = ICurveMetapool(_pool);
-        WETHID = _wEthId;
-        STETHID = _stEthId;
-        slippage = _slippage;
-
-        _name = "VaultCraft stEth Adapter";
-        _symbol = "vcStEth";
-
-        IERC20(_stEth).approve(_pool, type(uint256).max);
+        IERC20(address(lido)).approve(address(lido), type(uint256).max);
+        IERC20(address(lido)).approve(
+            address(StableSwapSTETH),
+            type(uint256).max
+        );
+        IERC20(address(weth)).approve(
+            address(StableSwapSTETH),
+            type(uint256).max
+        );
+        IERC20(address(weth)).approve(address(lido), type(uint256).max);
     }
+
+    //we get eth
+    receive() external payable {}
 
     function name()
         public
@@ -108,18 +118,16 @@ contract LidoAdapter is AdapterBase {
         return _symbol;
     }
 
-    // To receive eth from lido
-    receive() external payable {}
-
     /*//////////////////////////////////////////////////////////////
                             ACCOUNTING LOGIC
     //////////////////////////////////////////////////////////////*/
+
     function _underlyingBalance() internal view returns (uint256) {
         return lido.sharesOf(address(this));
     }
 
     function _totalAssets() internal view override returns (uint256) {
-        return IERC20(stEth).balanceOf(address(this)); // this can be higher than the total assets deposited due to staking rewards
+        return lido.balanceOf(address(this)); // this can be higher than the total assets deposited due to staking rewards
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -132,20 +140,20 @@ contract LidoAdapter is AdapterBase {
         uint256
     ) internal virtual override {
         weth.withdraw(assets); // Grab native Eth from Weth contract
-        ILido(stEth).submit{value: assets}(FEE_RECIPIENT); // Submit to Lido Contract
+        lido.submit{value: assets}(referal); // Submit to Lido Contract
     }
 
     /// @notice Withdraw from LIDO pool
     function _protocolWithdraw(
         uint256 assets,
-        uint256
+        uint256 shares
     ) internal virtual override {
         uint256 slippageAllowance = assets.mulDiv(
-            BPS_DENOMINATOR.sub(slippage),
-            BPS_DENOMINATOR,
+            DENOMINATOR.sub(slippage),
+            DENOMINATOR,
             Math.Rounding.Down
         );
-        uint256 amountRecieved = pool.exchange(
+        uint256 amountRecieved = StableSwapSTETH.exchange(
             STETHID,
             WETHID,
             assets,
@@ -153,7 +161,7 @@ contract LidoAdapter is AdapterBase {
         );
         weth.deposit{value: amountRecieved}(); // get wrapped eth back
     }
-    
+
     /**
      * @notice Withdraws `assets` from the underlying protocol and burns vault shares from `owner`.
      * @dev Executes harvest if `harvestCooldown` is passed since last invocation.
@@ -186,5 +194,29 @@ contract LidoAdapter is AdapterBase {
         harvest();
 
         emit Withdraw(caller, receiver, owner, assets, shares);
+    }
+
+    function _convertToShares(
+        uint256 assets,
+        Math.Rounding rounding
+    ) internal view virtual override returns (uint256) {
+        return
+            assets.mulDiv(
+                totalSupply() + 10 ** decimalOffset,
+                totalAssets() + 1,
+                rounding
+            );
+    }
+
+    function _convertToAssets(
+        uint256 shares,
+        Math.Rounding rounding
+    ) internal view virtual override returns (uint256) {
+        return
+            shares.mulDiv(
+                totalAssets() + 1,
+                totalSupply() + 10 ** decimalOffset,
+                rounding
+            );
     }
 }
