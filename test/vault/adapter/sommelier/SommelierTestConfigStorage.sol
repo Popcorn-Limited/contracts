1b881fc6547b807a0391b08a7f276e7dc28f38ca
amatureApe <amatureApe@gmail.com>
2023-07-26 16:18:45 -0700

    add adapter and testing suite

--- test/vault/adapter/sommelier/SommelierTestConfigStorage.sol
+++ test/vault/adapter/sommelier/SommelierTestConfigStorage.sol
@@ -5,22 +5,22 @@ pragma solidity ^0.8.15;
 
 import {ITestConfigStorage} from "../abstract/ITestConfigStorage.sol";
 
-struct SolidlyTestConfig {
-    address gauge;
+struct SommelierTestConfig {
+    address vault;
 }
 
-contract SolidlyTestConfigStorage is ITestConfigStorage {
-    SolidlyTestConfig[] internal testConfigs;
+contract SommelierTestConfigStorage is ITestConfigStorage {
+    SommelierTestConfig[] internal testConfigs;
 
     constructor() {
-        // Mainnet - wBTC
+        // Mainnet - wETH
         testConfigs.push(
-            SolidlyTestConfig(0x0274a704a6D9129F90A62dDC6f6024b33EcDad36)
+            SommelierTestConfig(0x84674cFFB6146D19b986fC88EC70a441b570A45B)
         );
     }
 
     function getTestConfig(uint256 i) public view returns (bytes memory) {
-        return abi.encode(testConfigs[i].gauge);
+        return abi.encode(testConfigs[i].vault);
     }
 
     function getTestConfigLength() public view returns (uint256) {
