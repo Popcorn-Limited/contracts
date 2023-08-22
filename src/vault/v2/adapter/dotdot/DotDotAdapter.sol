9063328391a2ea523fbfbc240be6f658b273d36e
mayowa <mtudonu85@gmail.com>
2023-08-22 15:00:48 +0100

    resolve comments

--- src/vault/v2/adapter/dotdot/DotDotAdapter.sol
+++ src/vault/v2/adapter/dotdot/DotDotAdapter.sol
@@ -13,16 +13,18 @@ contract DotDotAdapter is BaseAdapter {
     IDotDotStaking public lpStaking;
 
     error InvalidToken();
+    error LpTokenNotSupported();
 
     function __DotDotAdapter_init(
         AdapterConfig memory _adapterConfig,
         ProtocolConfig memory _protocolConfig
     ) internal onlyInitializing {
+        if(!_adapterConfig.useLpToken) revert LpTokenNotSupported();
         __BaseAdapter_init(_adapterConfig);
 
         lpStaking = IDotDotStaking(_protocolConfig.registry);
 
-        if (lpStaking.depositTokens(address (underlying)) == address(0))
+        if (lpStaking.depositTokens(address (lpToken)) == address(0))
             revert InvalidToken();
 
         _adapterConfig.lpToken.approve(address(lpStaking), type(uint256).max);
@@ -37,7 +39,7 @@ contract DotDotAdapter is BaseAdapter {
      * @dev This function must be overriden. If the farm requires the usage of lpToken than this function must convert lpToken balance into underlying balance
      */
     function _totalAssets() internal view override returns (uint256) {
-        return lpStaking.userBalances(address(this), address (underlying));
+        return lpStaking.userBalances(address(this), address (lpToken));
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -45,16 +47,16 @@ contract DotDotAdapter is BaseAdapter {
     //////////////////////////////////////////////////////////////*/
 
     function _deposit(uint256 amount) internal override {
-        underlying.safeTransferFrom(msg.sender, address(this), amount);
-        _depositUnderlying(amount);
+        lpToken.safeTransferFrom(msg.sender, address(this), amount);
+        _depositLP(amount);
     }
 
     /**
      * @notice Deposits underlying asset and converts it if necessary into an lpToken before depositing
      * @dev This function must be overriden. Some farms require the user to into an lpToken before depositing others might use the underlying directly
      **/
-    function _depositUnderlying(uint256 amount) internal override {
-        lpStaking.deposit(address(this), address (underlying), amount);
+    function _depositLP(uint256 amount) internal override {
+        lpStaking.deposit(address(this), address (lpToken), amount);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -62,16 +64,16 @@ contract DotDotAdapter is BaseAdapter {
     //////////////////////////////////////////////////////////////*/
 
     function _withdraw(uint256 amount, address receiver) internal override {
-        _withdrawUnderlying(amount);
-        underlying.safeTransfer(receiver, amount);
+        _withdrawLP(amount);
+        lpToken.safeTransfer(receiver, amount);
     }
 
     /**
      * @notice Withdraws underlying asset. If necessary it converts the lpToken into underlying before withdrawing
      * @dev This function must be overriden. Some farms require the user to into an lpToken before depositing others might use the underlying directly
      **/
-    function _withdrawUnderlying(uint256 amount) internal override {
-        lpStaking.withdraw(address(this), address(underlying), amount);
+    function _withdrawLP(uint256 amount) internal override {
+        lpStaking.withdraw(address(this), address(lpToken), amount);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -83,7 +85,7 @@ contract DotDotAdapter is BaseAdapter {
      */
     function _claim() internal override {
         address[] memory tokens = new address[](1);
-        tokens[0] = address (underlying);
+        tokens[0] = address (lpToken);
         try lpStaking.claim(address(this), tokens, 0) {
         } catch {}
     }
