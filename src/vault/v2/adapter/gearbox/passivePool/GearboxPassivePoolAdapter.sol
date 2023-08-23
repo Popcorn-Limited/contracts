e9a515d7b0a10dcd3605aed3b62496d83db2c596
mayowa <mtudonu85@gmail.com>
2023-08-23 12:16:43 +0100

    port gearbox to v2

---
+++ src/vault/v2/adapter/gearbox/passivePool/GearboxPassivePoolAdapter.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: GPL-3.0
+// Docgen-SOLC: 0.8.15
+
+pragma solidity ^0.8.15;
+
+import {
+    SafeERC20Upgradeable as SafeERC20
+} from "openzeppelin-contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import { IPoolService, IContractRegistry, IAddressProvider } from "../IGearbox.sol";
+import {BaseAdapter, IERC20, AdapterConfig, ProtocolConfig} from "../../../base/BaseAdapter.sol";
+
+
+contract BeefyAdapter is BaseAdapter {
+    using SafeERC20 for IERC20;
+
+    /// @notice The Pool Service Contract
+    IPoolService public poolService;
+
+    /// @notice The Diesel Token Contract
+    IERC20 public dieselToken;
+
+    error WrongPool();
+    error LpTokenNotSupported();
+
+    function __BeefyAdapter_init(
+        AdapterConfig memory _adapterConfig,
+        ProtocolConfig memory _protocolConfig
+    ) internal onlyInitializing {
+        if (_adapterConfig.useLpToken) revert LpTokenNotSupported();
+
+        __BaseAdapter_init(_adapterConfig);
+        (uint256 _pid, address addressProvider)= abi.decode(_protocolConfig.protocolInitData, (uint256, address));
+
+        poolService = IPoolService(IContractRegistry(IAddressProvider(addressProvider)
+            .getContractsRegister())
+            .pools(_pid));
+        dieselToken = IERC20(poolService.dieselToken());
+
+        if (address (underlying) != poolService.underlyingToken()) revert WrongPool();
+        _adapterConfig.underlying.approve(address (poolService), type(uint256).max);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            ACCOUNTING LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Returns the total amount of underlying assets.
+     * @dev This function must be overriden. If the farm requires the usage of lpToken than this function must convert lpToken balance into underlying balance
+     */
+    function _totalUnderlying() internal view override returns (uint256) {
+        uint256 _totalDieselTokens = dieselToken.balanceOf(address(this));
+
+        // roundUp to account for fromDiesel() ReoundDown
+        return _totalDieselTokens == 0 ? 0 : poolService.fromDiesel(_totalDieselTokens);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            DEPOSIT LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function _deposit(uint256 amount) internal override {
+        underlying.safeTransferFrom(msg.sender, address(this), amount);
+        _depositUnderlying(amount);
+    }
+
+    /**
+     * @notice Deposits underlying asset and converts it if necessary into an lpToken before depositing
+     * @dev This function must be overriden. Some farms require the user to into an lpToken before depositing others might use the underlying directly
+     **/
+    function _depositUnderlying(uint256 amount) internal override {
+        poolService.addLiquidity(amount, address(this), 0);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            WITHDRAWAL LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function _withdraw(uint256 amount, address receiver) internal override {
+        _withdrawUnderlying(amount);
+        underlying.safeTransfer(receiver, amount);
+    }
+
+    /**
+     * @notice Withdraws underlying asset. If necessary it converts the lpToken into underlying before withdrawing
+     * @dev This function must be overriden. Some farms require the user to into an lpToken before depositing others might use the underlying directly
+     **/
+    function _withdrawUnderlying(uint256 amount) internal override {
+        poolService.removeLiquidity(poolService.toDiesel(amount) + 1 + 1, address(this));
+    }
+}
