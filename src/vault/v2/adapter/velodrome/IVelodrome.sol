17015c77169fa1cd679fd3be82a30e8c964422fc
mayowa <mtudonu85@gmail.com>
2023-08-28 11:50:03 +0100

    port velodrome to v2

---
+++ src/vault/v2/adapter/velodrome/IVelodrome.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+// Docgen-SOLC: 0.8.15
+
+pragma solidity ^0.8.15;
+
+interface IGauge {
+    function stake() external view returns (address);
+
+    function balanceOf(address _user) external view returns (uint256);
+
+    function deposit(uint256 _amount, uint256 _tokenId) external;
+
+    function withdraw(uint256 _amount) external;
+
+    function getReward(address _account, address[] memory _tokens) external;
+
+    function rewards(uint256 _index) external view returns (address);
+}
+
+interface ILpToken {
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+}
