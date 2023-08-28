7c12590b02233d7f65ea7327271e76a8a02b2615
mayowa <mtudonu85@gmail.com>
2023-08-28 13:08:22 +0100

    refactor get reward tokens function

--- src/vault/v2/adapter/solidly/SolidlyAdapter.sol
+++ src/vault/v2/adapter/solidly/SolidlyAdapter.sol
@@ -98,11 +98,19 @@ contract GmdAdapter is BaseAdapter {
         } catch {}
     }
 
-    function _getRewardTokens() internal view returns(address[] memory) {
-        address[] memory _rewardTokens = new address[](3);
-        _rewardTokens[0] = address(rewardTokens[0]);
-        _rewardTokens[1] = address(rewardTokens[1]);
-        _rewardTokens[2] = address(rewardTokens[2]);
+    /**
+     * @notice Gets all the reward tokens for a protocol
+     * @dev This function converts all reward token types from IERC20[] to address[]
+     **/
+    function _getRewardTokens() internal virtual view returns(address[] memory) {
+        uint256 len = rewardTokens.length;
+        address[] memory _rewardTokens = new address[](len);
+        for(uint256 i = 0; i < len ;) {
+            _rewardTokens[i] = address(rewardTokens[i]);
+            unchecked {
+                i++;
+            }
+        }
 
         return _rewardTokens;
     }
