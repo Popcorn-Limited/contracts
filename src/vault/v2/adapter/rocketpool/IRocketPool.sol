41d796b59cd1a5f1803b6a1c64127106994cef82
mayowa <mtudonu85@gmail.com>
2023-09-01 13:14:35 +0100

    add rocket pool adapter to v2

---
+++ src/vault/v2/adapter/rocketpool/IRocketPool.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.15;
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+interface RocketStorageInterface {
+
+    // Deploy status
+    function getDeployedStatus() external view returns (bool);
+
+    // Guardian
+    function getGuardian() external view returns(address);
+    function setGuardian(address _newAddress) external;
+    function confirmGuardian() external;
+
+    // Getters
+    function getAddress(bytes32 _key) external view returns (address);
+    function getUint(bytes32 _key) external view returns (uint);
+    function getString(bytes32 _key) external view returns (string memory);
+    function getBytes(bytes32 _key) external view returns (bytes memory);
+    function getBool(bytes32 _key) external view returns (bool);
+    function getInt(bytes32 _key) external view returns (int);
+    function getBytes32(bytes32 _key) external view returns (bytes32);
+
+    // Setters
+    function setAddress(bytes32 _key, address _value) external;
+    function setUint(bytes32 _key, uint _value) external;
+    function setString(bytes32 _key, string calldata _value) external;
+    function setBytes(bytes32 _key, bytes calldata _value) external;
+    function setBool(bytes32 _key, bool _value) external;
+    function setInt(bytes32 _key, int _value) external;
+    function setBytes32(bytes32 _key, bytes32 _value) external;
+
+    // Deleters
+    function deleteAddress(bytes32 _key) external;
+    function deleteUint(bytes32 _key) external;
+    function deleteString(bytes32 _key) external;
+    function deleteBytes(bytes32 _key) external;
+    function deleteBool(bytes32 _key) external;
+    function deleteInt(bytes32 _key) external;
+    function deleteBytes32(bytes32 _key) external;
+
+    // Arithmetic
+    function addUint(bytes32 _key, uint256 _amount) external;
+    function subUint(bytes32 _key, uint256 _amount) external;
+
+    // Protected storage
+    function getNodeWithdrawalAddress(address _nodeAddress) external view returns (address);
+    function getNodePendingWithdrawalAddress(address _nodeAddress) external view returns (address);
+    function setWithdrawalAddress(address _nodeAddress, address _newWithdrawalAddress, bool _confirm) external;
+    function confirmWithdrawalAddress(address _nodeAddress) external;
+}
+
+interface RocketTokenRETHInterface is IERC20 {
+    function getEthValue(uint256 _rethAmount) external view returns (uint256);
+    function getRethValue(uint256 _ethAmount) external view returns (uint256);
+    function getExchangeRate() external view returns (uint256);
+    function getTotalCollateral() external view returns (uint256);
+    function getCollateralRate() external view returns (uint256);
+    function depositExcess() external payable;
+    function depositExcessCollateral() external;
+    function mint(uint256 _ethAmount, address _to) external;
+    function burn(uint256 _rethAmount) external;
+}
+
+interface RocketDepositPoolInterface {
+    function getBalance() external view returns (uint256);
+    function getNodeBalance() external view returns (uint256);
+    function getUserBalance() external view returns (int256);
+    function getExcessBalance() external view returns (uint256);
+    function deposit() external payable;
+    function getMaximumDepositAmount() external view returns (uint256);
+    function nodeDeposit(uint256 _totalAmount) external payable;
+    function nodeCreditWithdrawal(uint256 _amount) external;
+    function recycleDissolvedDeposit() external payable;
+    function recycleExcessCollateral() external payable;
+    function recycleLiquidatedStake() external payable;
+    function assignDeposits() external;
+    function maybeAssignDeposits() external returns (bool);
+    function withdrawExcessBalance(uint256 _amount) external;
+}
+
+interface IWETH {
+    function deposit() external payable;
+    function withdraw(uint256 amount) external;
+    function balanceOf(address) external view returns(uint256);
+}
+
+interface ICurveMetapool {
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external returns (uint256);
+}
---
+++ src/vault/v2/adapter/rocketpool/RocketPoolAdapter.sol
@@ -0,0 +1,145 @@
+// SPDX-License-Identifier: GPL-3.0
+// Docgen-SOLC: 0.8.15
+
+pragma solidity ^0.8.15;
+import "./IRocketPool.sol";
+import {UniswapV3Utils, IUniV3Pool} from "../../../../utils/UniswapV3Utils.sol";
+import {BaseAdapter, IERC20 as ERC20, AdapterConfig, ProtocolConfig} from "../../base/BaseAdapter.sol";
+import {MathUpgradeable as Math} from "openzeppelin-contracts-upgradeable/utils/math/MathUpgradeable.sol";
+import {SafeERC20Upgradeable as SafeERC20} from "openzeppelin-contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+
+
+contract RocketPoolAdapter is BaseAdapter {
+    using SafeERC20 for ERC20;
+    using Math for uint256;
+
+    address public uniRouter;
+    uint24 public uniSwapFee;
+
+    IWETH public wETH;
+    RocketStorageInterface public rocketStorage;
+    RocketTokenRETHInterface public rocketTokenRETH;
+    RocketDepositPoolInterface public rocketDepositPool;
+
+    error NoSharesBurned();
+    error InvalidAddress();
+    error LpTokenNotSupported();
+    error InsufficientSharesReceived();
+
+    function __RocketPoolAdapter_init(
+        AdapterConfig memory _adapterConfig,
+        ProtocolConfig memory _protocolConfig
+    ) internal onlyInitializing {
+        if (_adapterConfig.useLpToken) revert LpTokenNotSupported();
+        __BaseAdapter_init(_adapterConfig);
+
+        (
+            address _rocketStorageAddress,
+            address _wETH,
+            address _uniRouter,
+            uint24 _uniSwapFee
+        ) = abi.decode(
+            _protocolConfig.protocolInitData, (address, address, address , uint24 )
+        );
+        wETH = IWETH(_wETH);
+        rocketStorage = RocketStorageInterface(_rocketStorageAddress);
+
+        address rocketDepositPoolAddress = rocketStorage.getAddress(
+            keccak256(abi.encodePacked("contract.address", "rocketDepositPool"))
+        );
+        address rocketTokenRETHAddress = rocketStorage.getAddress(
+            keccak256(abi.encodePacked("contract.address", "rocketTokenRETH"))
+        );
+
+        if(
+            address(rocketDepositPoolAddress) == address(0) ||
+            address(rocketTokenRETHAddress) == address(0)
+        ) revert InvalidAddress();
+
+        rocketDepositPool = RocketDepositPoolInterface(rocketDepositPoolAddress);
+        rocketTokenRETH = RocketTokenRETHInterface(rocketTokenRETHAddress);
+
+        rocketTokenRETH.approve(rocketTokenRETHAddress, type(uint256).max);
+        rocketTokenRETH.approve(uniRouter, type(uint256).max);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            ACCOUNTING LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Returns the total amount of underlying assets.
+     * @dev This function must be overriden. If the farm requires the usage of lpToken than this function must convert lpToken balance into underlying balance
+     */
+    function _totalUnderlying() internal view override returns (uint256) {
+        return rocketTokenRETH.getEthValue(rocketTokenRETH.balanceOf(address (this)));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            DEPOSIT LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function _deposit(uint256 amount) internal override {
+        underlying.safeTransferFrom(msg.sender, address(this), amount);
+        _depositUnderlying(amount);
+    }
+
+    /**
+     * @notice Deposits underlying asset and converts it if necessary into an lpToken before depositing
+     * @dev This function must be overridden. Some farms require the user to into an lpToken before depositing
+     *      others might use the underlying directly
+     **/
+    function _depositUnderlying(uint256 amount) internal override {
+        wETH.withdraw(amount);
+        rocketDepositPool.deposit{value: amount}();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            WITHDRAWAL LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function _withdraw(uint256 amount, address receiver) internal override {
+        _withdrawUnderlying(amount);
+        underlying.safeTransfer(receiver, amount);
+    }
+
+    /**
+     * @notice Withdraws underlying asset. If necessary it converts the lpToken into underlying before withdrawing
+     * @dev This function must be overridden. Some farms require the user to into an lpToken before depositing others might use the underlying directly
+     **/
+    function _withdrawUnderlying(uint256 amount) internal override {
+        uint256 rETHShares = convertToUnderlyingShares(amount);
+        if(rocketTokenRETH.getTotalCollateral()
+            > rocketTokenRETH.getEthValue(rETHShares)
+        ) {
+            rocketTokenRETH.burn(rETHShares);
+            wETH.deposit{value: amount}();
+            return;
+        }
+
+        //if there isn't enough ETH in the rocket pool, we swap rETH directly for WETH
+        UniswapV3Utils.swap(
+            uniRouter,
+            address(rocketTokenRETH),
+            address(underlying),
+            uniSwapFee,
+            rETHShares
+        );
+    }
+
+    function convertToUnderlyingShares(
+        uint256 shares
+    ) public view returns (uint256) {
+        uint256 supply = _totalUnderlying();
+        return
+            supply == 0
+                ? shares
+                : shares.mulDiv(
+                rocketTokenRETH.balanceOf(address(this)),
+                supply,
+                Math.Rounding.Up
+            );
+    }
+
+    receive() external payable {}
+}
