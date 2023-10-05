2784185c1195f2ea3ff81b1dff566ee55c244d1d
Ruhum <ruhum@proton.me>
2023-10-05 14:02:40 +0200

    chore(DeployInfra): add script to deploy v2 infra

---
+++ script/DeployInfra.s.sol
@@ -0,0 +1,71 @@
+import {CREATE3Script} from "./base/CREATE3Script.sol";
+
+import {AdminProxy} from "../src/vault/AdminProxy.sol";
+import {VaultFactory} from "../src/vault/VaultFactory.sol";
+import {VaultRegistry} from "../src/vault/VaultRegistry.sol";
+import {TemplateRegistry} from "../src/vault/TemplateRegistry.sol";
+
+contract DeployInfra is CREATE3Script {
+    constructor() CREATE3Script(vm.envString("VERSION")) {}
+
+    function run() public returns (
+        AdminProxy adminProxy,
+        VaultFactory vaultFactory,
+        VaultRegistry vaultRegistry,
+        TemplateRegistry templateRegistry
+    ) {
+        address deployer = vm.envAddress("DEPLOYER");
+    
+        vm.startBroadcast(deployer);
+    
+        adminProxy = AdminProxy(
+            create3.deploy(
+                getCreate3ContractSalt("AdminProxy"),
+                bytes.concat(
+                    type(AdminProxy).creationCode,
+                    abi.encode(deployer)
+                )
+            )
+        );
+
+        vaultRegistry = VaultRegistry(
+            create3.deploy(
+                getCreate3ContractSalt("VaultRegistry"),
+                bytes.concat(
+                    type(VaultRegistry).creationCode,
+                    // VaultRegistry is owned by factory
+                    abi.encode(getCreate3Contract("VaultFactory"))
+                )
+            )
+        );
+
+        templateRegistry = TemplateRegistry(
+            create3.deploy(
+                getCreate3ContractSalt("TemplateRegistry"),
+                bytes.concat(
+                    type(TemplateRegistry).creationCode,
+                    // TemplateRegistry is owned by AdminProxy
+                    abi.encode(address(adminProxy))
+                )
+            )
+        );
+
+        vaultFactory = VaultFactory(
+            create3.deploy(
+                getCreate3ContractSalt("VaultFactory"),
+                bytes.concat(
+                    type(VaultFactory).creationCode,
+                    abi.encode(address(adminProxy), address(vaultRegistry), address(templateRegistry))
+                )
+            )
+        );
+    }
+
+    // deploy vault factory
+
+    // deploy vault registry
+
+    // deploy template registry
+
+    // deploy admin proxy
+}
