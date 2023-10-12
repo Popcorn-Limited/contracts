5f6675b97ab0dee90ead24d65f4a26c6fee1e976
mayowa <mtudonu85@gmail.com>
2023-10-12 23:18:07 +0400

    remove used v1 files

--- src/interfaces/vault/ITemplateRegistry.sol
+++ src/interfaces/vault/ITemplateRegistry.sol
@@ -6,7 +6,7 @@ import { IOwned } from "../IOwned.sol";
 
 interface ITemplateRegistry is IOwned {
   function templates(bytes32 version, address template) external view returns (bool);
-  function allTemplates(bytes32 version, bytes32 category) external view returns (address[] memory;)
+  function allTemplates(bytes32 version, bytes32 category) external view returns (address[] memory);
   function addTemplate(
     bytes32 version,
     bytes32 category,
