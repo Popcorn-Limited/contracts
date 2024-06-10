6a6de724982e2c298e77cab2f5ae3c5c8be90f1f
Andrea Di Nenno <dinennoandrea@gmail.com>
2024-06-10 09:50:05 +0200

    Add peapods strategies using UniV2 and Balancer Lp compounder

---
+++ src/peripheral/BaseUniV2Compounder.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: GPL-3.0
+// Docgen-SOLC: 0.8.25
+
+pragma solidity ^0.8.25;
+
+import {UniswapV2TradeLibrary, IUniswapRouterV2} from "./UniswapV2TradeLibrary.sol";
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import {Math} from "openzeppelin-contracts/utils/math/Math.sol";
+
+struct SwapStep {
+    address[] path;
+}
+
+abstract contract BaseUniV2Compounder {
+    using SafeERC20 for IERC20;
+    using Math for uint256;
+
+    IUniswapRouterV2 public uniswapRouter;
+    
+    address[] public sellTokens;
+    SwapStep[] internal sellSwaps; // for each sellToken there are two paths.
+
+    // sell half the rewards for a pool tokenA and half for the tokenB
+    function sellRewardsForBaseTokensViaUniswapV2() internal {
+        // caching
+        IUniswapRouterV2 router = uniswapRouter;
+        SwapStep memory swap;
+
+        uint256 rewLen = sellTokens.length;
+        for (uint256 i = 0; i < rewLen;) {
+            uint256 totAmount = IERC20(sellTokens[i]).balanceOf(address(this));
+
+            // sell half for tokenA
+            swap = sellSwaps[2 * i];
+            uint256 amount = totAmount.mulDiv(1e18, 2e18, Math.Rounding.Floor);
+            if (amount > 0) {
+                UniswapV2TradeLibrary.trade(router, swap.path, address(this), block.timestamp, amount, 0);
+            }
+
+            // sell the rest for tokenB
+            swap = sellSwaps[2 * i + 1];
+            amount = totAmount - amount;
+            if (amount > 0) {
+                UniswapV2TradeLibrary.trade(router, swap.path, address(this), block.timestamp, amount, 0);
+            }
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    // sell all rewards for a single token
+    function sellRewardsViaUniswapV2() internal {
+        IUniswapRouterV2 router = uniswapRouter;
+        SwapStep memory swap;
+
+        uint256 rewLen = sellTokens.length;
+        for (uint256 i = 0; i < rewLen;) {
+            uint256 totAmount = IERC20(sellTokens[i]).balanceOf(address(this));
+            swap = sellSwaps[i];
+            
+            if (totAmount > 0) {
+                UniswapV2TradeLibrary.trade(router, swap.path, address(this), block.timestamp, totAmount, 0);
+            }
+            
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function setUniswapTradeValues(
+        address newRouter, 
+        address[] memory rewTokens, 
+        SwapStep[] memory newSwaps
+    ) internal {
+        // Remove old rewardToken allowance
+        uint256 sellTokensLen = sellTokens.length;
+        if (sellTokensLen > 0) {
+            // caching
+            address oldRouter = address(uniswapRouter);
+            address[] memory oldSellTokens = sellTokens;
+
+            // void approvals
+            for (uint256 i = 0; i < sellTokensLen;) {
+                IERC20(oldSellTokens[i]).forceApprove(oldRouter, 0);
+
+                unchecked {
+                    ++i;
+                }
+            }
+        }
+
+        // delete old state
+        delete sellTokens;
+        delete sellSwaps;
+
+        // Add new allowance + state
+        address newRewardToken;
+        sellTokensLen = rewTokens.length;
+        for (uint256 i = 0; i < sellTokensLen;) {
+            newRewardToken = rewTokens[i];
+
+            IERC20(newRewardToken).forceApprove(newRouter, type(uint256).max);
+
+            sellTokens.push(newRewardToken);
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        for (uint256 i=0; i<newSwaps.length;) {
+            sellSwaps.push(newSwaps[i]);
+            unchecked {
+                ++i;
+            }
+        }
+
+        uniswapRouter = IUniswapRouterV2(newRouter);
+    }
+}
---
+++ src/strategies/peapods/PeapodsUniswapV2Compounder.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: GPL-3.0
+// Docgen-SOLC: 0.8.15
+
+pragma solidity ^0.8.15;
+
+import {PeapodsDepositor, IERC20, SafeERC20} from "./PeapodsStrategy.sol";
+import {BaseUniV2LpCompounder, SwapStep} from "../../peripheral/BaseUniV2LpCompounder.sol";
+
+/**
+ * @title   ERC4626 Peapods Protocol Vault Adapter
+ * @author  ADN
+ * @notice  ERC4626 wrapper for Peapods protocol
+ *
+ * An ERC4626 compliant Wrapper for Peapods.
+ * Implements harvest func that swaps via Uniswap V2
+ */
+contract PeapodsDepositorUniswapV2Compounder is PeapodsDepositor, BaseUniV2LpCompounder{
+    using SafeERC20 for IERC20;
+
+    /*//////////////////////////////////////////////////////////////
+                            INITIALIZATION
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Initialize a new Strategy.
+     * @param asset_ The underlying asset used for deposit/withdraw and accounting
+     * @param owner_ Owner of the contract. Controls management functions.
+     * @param autoDeposit_ Controls if `protocolDeposit` gets called on deposit
+     * @param strategyInitData_ Encoded data for this specific strategy
+     */
+    function initialize(address asset_, address owner_, bool autoDeposit_, bytes memory strategyInitData_)
+        external
+        override
+        initializer
+    {
+        __PeapodsBase_init(asset_, owner_, autoDeposit_, strategyInitData_);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            REWARDS LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice The token rewarded from the pendle market
+    function rewardTokens() external view override returns (address[] memory) {
+        return _getRewardTokens();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            HARVESGT LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Claim rewards, swaps to asset and add liquidity
+     */
+    function harvest(bytes memory data) external override onlyKeeperOrOwner {
+        claim();
+        
+        // caching
+        address asset_ = asset();
+
+        sellRewardsForLpTokenViaUniswap(asset_, address(this), block.timestamp, data);
+
+        _protocolDeposit(IERC20(asset_).balanceOf(address(this)), 0, data);
+
+        emit Harvested();
+    }
+
+    function setHarvestValues(
+        address newRouter,
+        address[2] memory newDepositAssets,
+        SwapStep[] memory newSwaps
+    ) external onlyOwner {
+        setUniswapLpCompounderValues(newRouter, newDepositAssets, _getRewardTokens(), newSwaps);
+    }
+
+    // allow owner to withdraw eventual dust amount of tokens 
+    // from the compounding operation
+    function withdrawDust(address token) external onlyOwner {
+        if(token != depositAssets[0] && token != depositAssets[1])
+            revert("Invalid Token");
+
+        IERC20(token).safeTransfer(owner, IERC20(token).balanceOf(address(this)));
+    }
+
+    function _getRewardTokens() internal view returns (address[] memory tokens) {
+        tokens = new address[](1);
+        tokens[0] = poolRewards.rewardsToken();
+    }
+}
