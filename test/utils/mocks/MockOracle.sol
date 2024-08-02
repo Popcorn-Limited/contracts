05257d529229824b80e621ec26066557515da7f9
Ruhum <ruhum@proton.me>
2024-08-02 12:44:19 +0300

    test(MockOracle): add inverse price

--- test/utils/mocks/MockOracle.sol
+++ test/utils/mocks/MockOracle.sol
@@ -8,9 +8,10 @@ contract MockOracle {
 
     function setPrice(address base, address quote, uint256 price) external {
         prices[base][quote] = price;
+        prices[quote][base] = 1e18 * 1e18 / price;
     }
 
     function getQuote(uint inAmount, address base, address quote) external view returns (uint) {
-        return prices[base][quote] == 0 ? inAmount : prices[base][quote];
+        return prices[base][quote] == 0 ? inAmount : prices[base][quote] * inAmount / 1e18;
     }
 }
