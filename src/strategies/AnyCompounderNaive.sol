27ffa56b42ea43e5e47d2e00991eecade8c46728
Ruhum <ruhum@proton.me>
2024-08-09 07:20:42 +0300

    fix(AnyConverter): add SafeERC20 library

--- src/strategies/AnyCompounderNaive.sol
+++ src/strategies/AnyCompounderNaive.sol
@@ -3,6 +3,7 @@
 
 pragma solidity ^0.8.25;
 
+import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 import {AnyConverter, IERC20Metadata, ERC20, IERC20, Math} from "./AnyConverter.sol";
 
 /**
@@ -17,6 +18,7 @@ import {AnyConverter, IERC20Metadata, ERC20, IERC20, Math} from "./AnyConverter.
  */
 abstract contract AnyCompounderNaive is AnyConverter {
     using Math for uint256;
+    using SafeERC20 for IERC20;
 
     address[] public _rewardTokens;
 
--- src/strategies/AnyConverter.sol
+++ src/strategies/AnyConverter.sol
@@ -3,6 +3,7 @@
 
 pragma solidity ^0.8.25;
 
+import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 import {BaseStrategy, IERC20Metadata, ERC20, IERC20, Math} from "./BaseStrategy.sol";
 import {IPriceOracle} from "src/interfaces/IPriceOracle.sol";
 
@@ -18,6 +19,7 @@ import {IPriceOracle} from "src/interfaces/IPriceOracle.sol";
  */
 abstract contract AnyConverter is BaseStrategy {
     using Math for uint256;
+    using SafeERC20 for IERC20; 
 
     address public yieldAsset;
     address[] public tokens;
