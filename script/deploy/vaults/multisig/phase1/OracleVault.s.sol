3d97fe0daa215b05ab8c9fb25156e54d6741da00
RedVeil <leon.niesler@gmail.com>
2024-11-25 16:43:15 +0100

    adding optimism lido vault

--- script/deploy/vaults/multisig/phase1/OracleVault.s.sol
+++ script/deploy/vaults/multisig/phase1/OracleVault.s.sol
@@ -26,18 +26,19 @@ contract Deploy is Script {
         vm.startBroadcast();
         console.log("msg.sender:", msg.sender);
 
-        // @dev edit this values below
-        asset = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+        name = "Safe Vault";
+        symbol = "sVault";
 
-        name = "Oracle Vault";
-        symbol = "oVault";
+        // @dev edit the values below
+        asset = 0x4200000000000000000000000000000000000006;
 
         limits = Limits(type(uint256).max, 0);
         fees = Fees(0, 0, 0, 0, 0, msg.sender);
 
-        oracle = 0xf7C42Db8bdD563539861de0ef2520Aa80c28e8c4;
+        oracle = 0xa7df2Ff7a6E1FAEb480617A01aD80b99CE39Bcc3;
 
-        multisig = 0x3C99dEa58119DE3962253aea656e61E5fBE21613;
+        // Lido arb,opt
+        multisig = 0x158a006d5FBA167C8c439e010343e0603DC44847;
 
         owner = msg.sender;
 
